c8eb0d5bdde8591f1184eb13f5e22bf59a5847d5
liuxiangwei <1670452415@qq.com>
2016-11-09 18:23:21 +0800

    created.

---
+++ NestSound_Swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NestSound_Swift
+//
+//  Created by yintao on 2016/11/9.
+//  Copyright © 2016年 yintao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+#Podfile
+platform :ios, '9.0'
+use_frameworks!
+
+target 'NestSound_Swift' do
+  pod 'Alamofire', '~> 4.0'
+  pod 'SwiftyJSON'
+  pod 'HandyJSON', '~> 1.2.0'
+  pod 'Kingfisher', '~> 3.0'
+end
